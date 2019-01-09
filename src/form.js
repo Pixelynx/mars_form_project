34c35d29007fa0ec1503aa974a0e3b4215713289
Pixelynx <tjstevens1992@gmail.com>
2019-01-09 12:44:01 -0500

    date added

--- src/form.js
+++ src/form.js
@@ -10,35 +10,67 @@ class Form extends Component {
 
   handleChange = (event) => {
     this.setState({
-      name: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
-  years = () => {
-    let year = document.querySelector('.year');
-    for (let i = 0; i <= 1990; i++) {
-      let option = document.createElement('option');
-      option.innerText = i;
-      year.appendChild(option)
+
+  months = () => {
+    let arr = [];
+    let mo = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    for(let i = 0; i <= 11; i++) {
+      arr.push(<option>{mo[i]}</option>)
     }
+    return arr;
   }
+
+  days = () => {
+    let arr = [];
+    for(let i = 1; i <= 31; i++) {
+      arr.push(<option>{i}</option>)
+    }
+    return arr;
+  }
+
+
+
+  years = () => {
+    let arr = [];
+    for (let i = 2019; i >= 1990; i--) {
+      arr.push(<option>{i}</option>)
+    }
+    return arr;
+  }
+
 
   render() {
     console.log(this.state);
+    let years = this.years();
+    let days = this.days();
+    let months = this.months();
     return(
       <div className="register">
         <form>
           <label htmlFor="name">Name: </label>
           <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+
           <br />
+
           <label htmlFor="dob">DOB: </label>
-          <select className="day" type="day" name="day">
-            <option name="" value=""></option>
+          <select className="day" name="day">
+            {days}
           </select>
-          <select className="month" type="month" name="month">
-            <option name="" value=""></option>
+          <select className="month" name="month">
+            {months}
           </select>
-          <select className="year" type="year" name="year">
+          <select className="year" name="year">
+            {years}
+          </select>
+
+          <br />
+
+          <label htmlFor="country">Home Country: </label>
+          <select className="country" name="country">
             <option name="" value=""></option>
           </select>
         </form>
